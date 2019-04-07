cee9d872a92fd4d7733af73e7972c93bd8e6fb9d
enesbugrasandal <49374427+enesbugrasandal@users.noreply.github.com>
2019-04-07 21:58:45 +0300

    Add files via upload

---
+++ Ödev 5/Ödev 6/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayi, s;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            s = rnd.Next(101);
+            sayi = 100;
+            label2.Text = sayi.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sayi = sayi + (sayi / 2);
+            label2.Text = sayi.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sayi = sayi - (sayi / 2);
+            label2.Text = sayi.ToString();
+        }
+    }
+}
